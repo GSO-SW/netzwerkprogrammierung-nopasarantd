5f9ebef4cd33594d030979e5c4992b0a8c733c46
Alexander Patola <alexanderpatola@gmail.com>
2022-02-13 17:18:45 +0100

    BalloonType wurde hinzugefügt. Die Klasse Balloon wurde mit einigen Membern hinzugefügt.

---
+++ NoPasaranTD/Model/Balloon.cs
@@ -0,0 +1,46 @@
+﻿namespace NoPasaranTD.Model
+{
+    /// <summary>
+    /// Model Klasse eines Ballon Objektes
+    /// </summary>
+    public class Balloon
+    {
+        public Balloon(BalloonType type)
+        {
+            ballonType = type;
+        }
+      
+        public Balloon() { }
+        
+        private BalloonType ballonType = BalloonType.None;
+        /// <summary>
+        /// Der Ballon Type mithilfe dessen weitere Memberwerte bestimmt werden
+        /// </summary>
+        public BalloonType Type { get => ballonType; set => ballonType = value; }
+
+        /// <summary>
+        /// Die derzeitige Position des Ballones auf dem Pfad in Prozentangabe
+        /// </summary>
+        public uint PathPosition { get; set; }
+
+        /// <summary>
+        /// Die Stärke des Ballones (Abhängig vom Ballon Typen)
+        /// </summary>
+        public uint Strength { get /* Zurgiff auf StaticInfo */; }
+
+    }
+
+    /// <summary>
+    /// Typen eines Ballones
+    /// </summary>
+    public enum BalloonType
+    {
+        None,
+        Rot,
+        Blau,
+        Grün,
+        Lila,
+        Schawrz,
+        Gold,
+    }
+}
