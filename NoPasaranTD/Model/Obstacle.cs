68c70668b2c760d5457b1ca61078f90cf25ffe61
Muck3334444 <larichgo@web.de>
2022-02-14 18:01:19 +0100

    Obstacle Modell hinzugefügt
    Modellklasse für Obstacles hinzugefügt und eine Enumeration für den ObstacleType erstellt.

--- NoPasaranTD/Model/Obstacle.cs
+++ NoPasaranTD/Model/Obstacle.cs
@@ -1,12 +1,32 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace NoPasaranTD.Model
 {
-    internal class Obstacle
+    public class Obstacle
     {
+        public Obstacle(ObstacleType obstacleType, Rectangle hitbox)
+        {
+            ObstacleType = obstacleType;
+            Hitbox = hitbox;
+        }
+
+        public ObstacleType ObstacleType { get; set; }
+        public Rectangle Hitbox { get; set; } // Maybe change to RectangleF
+
+        public Bitmap Image { get; } // Link with static class
+    }
+
+    public enum ObstacleType
+    {
+        House0 = 0,
+        House1 = 1,
+        Tree = 2,
+        Rock = 3,
+        Fence = 4
     }
 }
