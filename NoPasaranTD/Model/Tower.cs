02b6a676936f2ef966c72ddf356b8dfc8d2af5c9
Tobias Hoffstadt <tobias.h9@gso.schule.koeln>
2022-02-15 12:55:19 +0100

    (TD-11) Model Class Tower erstellt

---
+++ NoPasaranTD/Model/Tower.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NoPasaranTD.Model
+{
+    public abstract class Tower
+    {
+        public Rectangle Hitbox { get; set; }
+        public uint Level { get; set; }
+        public uint Strength { get; }
+        public uint Speed { get; }
+        public uint Range { get; }
+        public abstract void Render();
+        public abstract void Update();
+    }
+}
