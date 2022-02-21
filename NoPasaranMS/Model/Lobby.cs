f7663da286ffd17f4d99ebc471a6293ae30cbcf6
Paolo V <paolo.v@gso.schule.koeln>
2022-02-21 20:56:55 +0100

    (TD-37) Projekt und Basisstruktur erstellt

---
+++ NoPasaranMS/Model/Lobby.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NoPasaranMS.Model
+{
+    public class Lobby
+    {
+    }
+}
---
+++ NoPasaranMS/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace NoPasaranMS
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+    }
+}
