5a5eb16a1e3d2e42abaa761311a9922513aa1b1d
Muck3334444 <larichgo@web.de>
2022-02-27 15:58:10 +0100

    (TD-57) Veränderung der Kollisionskontrolle mit dem Pfad
    In der Map wurde eine Eigenschaft hinzugefügt, welche Vektoren speichert die Orthogonal mit einem definierten Abstand von dem Pfad entfernt sind. Dadurch kann teilweise eine Hitbox erstellt werden.
    Eine neue Methode wurde dafür in der Map Klasse hinzugefügt, um diese Punkte zu berechnen und zu speichern.
    Die Methode der Abstandskontrolle in der Game Klasse wurde so angepasst, dass nicht der Radius um die Ecken des Rechtecks kontrolliert werden, sondern die einzelnen Eckpunkte des Pfades. Dazu wurde eine Kontrolle in die Methode eingefügt, für eine Kollision zwischen der Hitbox und den Recheckseiten. Dadurch ist um den gesamten Ballonpfad herum eine Hitbox.

--- NoPasaranTD/Engine/Game.cs
+++ NoPasaranTD/Engine/Game.cs
@@ -41,36 +41,68 @@ namespace NoPasaranTD.Engine
 		}
 
 		/// <summary>
-		/// Kontrolliert, ob das angegebene Recheck einen Abstand von 50 Einheiten zum nächsten Pfad hat 
+		/// Kontrolliert, ob das angegebene Recheck einen Abstand von X Einheiten zum nächsten Pfadpunkt hat und ob keine Hitbox des Pfades getroffen wurde
 		/// </summary>
 		/// <param name="rect">Zu überprüfendes Rechteck</param>
 		/// <returns>False wenn es eine Überschneidung gibt</returns>
-		public bool TowerCollisionPath(Rectangle rect)
+		public bool TowerCollisionPathCorner(Rectangle rect)
         {
-            for (int i = 0; i < CurrentMap.BalloonPath.Length - 1; i++) // Alle Pfadteile durchgehen
-            {
-				Vector2D positionV = CurrentMap.BalloonPath[i]; // Ortsvektor der Gerade die den Pfadabschnitt darstellt
-				Vector2D connectionV = CurrentMap.BalloonPath[i + 1] - CurrentMap.BalloonPath[i]; // Verbindungsvektor der Gerade die den Pfadabschnitt darstellt
-                for (int j = 0; j < 2; j++) // Durchgehen beider X Möglichkeiten der verschiebung vom Eckpunkt oben links
-                {
-                    for (int k = 0; k < 2; k++) // Durchgehen beider Y Möglichkeiten
-                    {
-						Vector2D rectangleCornerV = new Vector2D(rect.X + j * rect.Width, rect.Y + k * rect.Height); // Verschiebung des Eckpunktes zur kontrolle
-						// Lotfußpunktverfahren zu einer Formel umgestellt
-						float closestPointDistance = -1 * (((positionV.X - rectangleCornerV.X) * connectionV.X + (positionV.Y - rectangleCornerV.Y) * connectionV.Y) / ((connectionV.X * connectionV.X) + (connectionV.Y * connectionV.Y)));
-						//Bestimmen des Punkts auf dem Pfad der am nächsten am Eckpunkt ist
-						Vector2D closestV = new Vector2D(positionV.X + closestPointDistance * connectionV.X, positionV.Y + closestPointDistance * connectionV.Y);
+            Vector2D[] cornersV = new Vector2D[3]; // Speichern der Ecken des Rechtecks
+            for (int i = 0; i < 2; i++) // Alle Ecken durchgehen
+                for (int j = 0; j < 2; j++)
+                    cornersV[i * 2 + j] = new Vector2D(rect.X + i * rect.Width, rect.Y + j * rect.Height); // Ecken abspeichern
+            Vector2D save = cornersV[2]; // Ecke 2 mit 3 tauschen um eine durchgehende Reihenfolge zu haben
+            cornersV[2] = cornersV[3];
+            cornersV[3] = save;
 
-						// Länge der kürzesten Verbindung bestimmen
-						if ((closestV - rectangleCornerV).Magnitude < 50) // TODO: Wert an StaticInfo festmachen
+            for (int i = 0; i < cornersV.Length; i++) // Alle Ecken durchgehen
+            {
+                Vector2D connection;
+                if (i != cornersV.Length - 1) // Die connection ist immer mit dem nächsten Punkt in der Reihe
+                    connection = cornersV[i] - cornersV[i + 1];
+                else // Bei dem letzten punkt wieder auf den ersten springen
+                    connection = cornersV[i] - cornersV[0];
+
+                // Durchgehen aller Pfadpunkte und schauen ob innerhalb des Radius ein Stück des Rechtecks ist
+                foreach (var item in CurrentMap.BalloonPath)
+                {
+                    // Nächster Punkt auf der Gerade des Rechtecks berechnet
+                    float closestPointDistance = -1 * (((cornersV[i].X - item.X) * connection.X + (cornersV[i].Y - item.Y) * connection.Y) / (connection.X * connection.X + connection.Y + connection.Y));
+                    if (closestPointDistance < 0) // Sollte der Punkt außerhalb der Länge des Rechtecks liegen wird auf die entsprechende Seite gesetzt
+                        closestPointDistance = 0;
+                    else if (closestPointDistance > 1)
+                        closestPointDistance = 1;
+
+                    if ((closestPointDistance * connection).Magnitude < 100) // Länge des Verbindungsvektors überprüfen // TODO: Mit StaticInfo verbinden
+                        return false;
+                }
+
+                // Alle Hitboxen des Pfades durchgehen und auf Kollisionen kontrollieren
+                for (int j = 0; j < CurrentMap.BallonPathHitbox.GetLength(1) - 1; j++)
+                {
+                    for (int k = 0; k < 2; k++) // Hitbox unter und oberhalb des Pfades checken
+                    {
+                        Vector2D currentV = CurrentMap.BallonPathHitbox[k, j]; // Ortsvektor des Hitboxteils
+                        Vector2D pathV = CurrentMap.BallonPathHitbox[k, j + 1] - currentV; // Richtungsvektor des Hitbox Teils
+                        // Kontrolle, ob die Rechteckseite und der Pfad kollinear sind
+                        float collinearF = connection.X * pathV.Y - connection.Y * pathV.X;
+                        if (collinearF != 0) // Nicht kollinear
                         {
-							return false;
+                            // Schnittpunkt Wert bestimmen
+                            float collisionF = ((currentV.Y - cornersV[i].Y) * connection.X + (currentV.X - cornersV[i].X) * connection.Y) / collinearF;
+                            if (collisionF < 1 && collisionF > 0) // Kontrolle, ob der Schnittpunkt im Intervall der Gerade liegt
+                                return false;
+                        }
+                        else // kollinear
+                        {
+                            float checkF = (currentV.X - cornersV[i].X) / pathV.X; // Kontrolle, ob die Geraden der Hitbox und des Rechtecks gleich sind
+                            if (checkF == (currentV.Y - cornersV[i].Y) / pathV.Y && checkF > 0 && checkF < 1) // Gleiche Gerade und innerhalb des Intervals
+                                return false;
                         }
                     }
                 }
-               
             }
-			return true;
+            return true;
         }
 	}
 }
