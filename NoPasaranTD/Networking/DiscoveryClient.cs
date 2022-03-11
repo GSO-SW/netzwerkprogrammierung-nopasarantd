08e9bb13752e3717e227d2c629d9355733535a87
Paolo V <paolo.v@gso.schule.koeln>
2022-03-11 21:10:02 +0100

    (TD-71) Kommentare eingefügt

--- NoPasaranTD/Networking/DiscoveryClient.cs
+++ NoPasaranTD/Networking/DiscoveryClient.cs
@@ -10,14 +10,36 @@ namespace NoPasaranTD.Networking
 {
     public class DiscoveryClient : IDisposable
     {
+        #region Property region
+        /// <summary>
+        /// Gibt an ob die derzeit besuchte Lobby gestartet wurde
+        /// </summary>
         public bool GameStarted { get; private set; } = false;
+
+        /// <summary>
+        /// Gibt an ob sich der Client authentifiziert hat
+        /// </summary>
         public bool LoggedIn { get; private set; } = false;
 
-        // Bleibt null bis eine Peer2Peer Verbindung aufgebaut wurde (GameStarted = true)
+        /// <summary>
+        /// Gibt die UDP Verbindung an, die ursprünglich als null-Referenz gilt.<br/>
+        /// Geändert wird dies sobald eine Peer2Peer Verbindung aufgebaut wurde,
+        /// weshalb man dies wirklich nur benutzen sollte wenn GameStarted = true ist
+        /// </summary>
         public UdpClient UdpClient { get; private set; }
+
+        /// <summary>
+        /// Gibt eine liste an NetworkClients die sich im Spiel befinden, die ursprünglich als null-Referenz gilt.<br/>
+        /// Geändert wird dies sobald eine Peer2Peer Verbindung aufgebaut wurde,
+        /// weshalb man dies wirklich nur benutzen sollte wenn GameStarted = true ist
+        /// </summary>
         public List<NetworkClient> Clients { get; private set; }
 
+        /// <summary>
+        /// Gibt die verfügbaren Lobbies an. Dies sollte nur genutzt werden, wenn LoggedIn = true ist
+        /// </summary>
         public List<NetworkLobby> Lobbies { get; }
+        #endregion
 
         private Socket TcpClient { get; }
         private StreamWriter TcpWriter { get; }
@@ -34,6 +56,11 @@ namespace NoPasaranTD.Networking
         }
 
         #region Command region
+        /// <summary>
+        /// Authentifiziert sich als den gegebenen Spieler<br/>
+        /// Ab hier fängt LoggedIn an sich auf "true" zu setzen
+        /// </summary>
+        /// <param name="player">Der spieler als den man sich authentifiziert</param>
         public async void LoginAsync(NetworkClient player)
         {
             string playerInfo = NetworkClient.Deserialize(player);
@@ -41,6 +68,10 @@ namespace NoPasaranTD.Networking
             await TcpWriter.FlushAsync();
         }
 
+        /// <summary>
+        /// Aktualisiert die Informationen des einzelnen Spielers
+        /// </summary>
+        /// <param name="player">Spezifizierter Spieler</param>
         public async void UpdatePlayer(NetworkClient player)
         {
             string playerInfo = NetworkClient.Deserialize(player);
@@ -48,30 +79,49 @@ namespace NoPasaranTD.Networking
             await TcpWriter.FlushAsync();
         }
 
+        /// <summary>
+        /// Erstellt eine neue Lobby indem man auch direkt hinzugefügt wird
+        /// </summary>
+        /// <param name="lobby">Spezifizierte Lobby</param>
         public async void CreateLobby(NetworkLobby lobby)
         {
             await TcpWriter.WriteLineAsync("NewLobby#" + lobby.GetInfo());
             await TcpWriter.FlushAsync();
         }
 
+        /// <summary>
+        /// Aktualisiert die gegebene Lobbyinformationen
+        /// </summary>
+        /// <param name="lobby">Spezifizierte Lobby</param>
         public async void UpdateLobby(NetworkLobby lobby)
         {
             await TcpWriter.WriteLineAsync("SetLobbyInfo#" + lobby.GetInfo());
             await TcpWriter.FlushAsync();
         }
 
+        /// <summary>
+        /// Tritt einer Lobby bei
+        /// </summary>
+        /// <param name="lobby">Spezifizierte Lobby</param>
         public async void JoinLobby(NetworkLobby lobby)
         {
             await TcpWriter.WriteLineAsync("Join#" + lobby.GetInfo());
             await TcpWriter.FlushAsync();
         }
 
+        /// <summary>
+        /// Verlässt die gerade beigetretene Lobby
+        /// </summary>
         public async void LeaveCurrentLobby()
         {
             await TcpWriter.WriteLineAsync("Leave#");
             await TcpWriter.FlushAsync();
         }
 
+        /// <summary>
+        /// Gibt den Befehl frei, das Spiel zu starten.<br/>
+        /// Ab hier fängt GameStarted an sich auf "true" zu setzen
+        /// </summary>
         public async void StartGame()
         {
             await TcpWriter.WriteLineAsync("StartGame#");
@@ -101,7 +151,7 @@ namespace NoPasaranTD.Networking
 
             Clients = new List<NetworkClient>();
             for (int i = 0; i < connectionArgs.Length / 2; i++)
-            {
+            { // Serialisiere die Antwort vom Server
                 NetworkClient client = NetworkClient.Serialize(connectionArgs[i]);
                 string[] endpointArgs = connectionArgs[i + 1].Split(':');
                 client.EndPoint = new IPEndPoint(
@@ -114,7 +164,7 @@ namespace NoPasaranTD.Networking
         }
 
         private void HandleInfo(string infoStr)
-        {
+        { // Aktualisiere die Lobbies
             Lobbies.Clear();
             string[] lobbyInfos = infoStr.Split('\t');
             for (int i = 0; i < lobbyInfos.Length; i++)
@@ -128,6 +178,7 @@ namespace NoPasaranTD.Networking
             {
                 while (true)
                 {
+                    // Warte auf Kommando vom Server
                     string message = TcpReader.ReadLine();
 
                     int index = message.IndexOf('#');
@@ -140,7 +191,7 @@ namespace NoPasaranTD.Networking
                     string paramStr = message.Substring(index + 1);
 
                     switch (command)
-                    {
+                    { // Verarbeite das Kommando vom Server
                         case "Info":
                             HandleInfo(paramStr);
                             break;
@@ -160,6 +211,7 @@ namespace NoPasaranTD.Networking
         public void Dispose()
         {
             TcpWriter.Dispose();
+            TcpReader.Dispose();
             TcpClient.Dispose();
         }
     }
