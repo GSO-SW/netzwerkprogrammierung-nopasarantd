59e37b9e913e6b7dabda3e1b9119031ff0a56b55
BruhBruhDev <78885863+BruhBruhDev@users.noreply.github.com>
2023-05-12 17:15:43 +0200

    Update Program.cs
    Server reagiert nicht / ist offline. IP auf localhost geändert.

--- NoPasaranTD/Program.cs
+++ NoPasaranTD/Program.cs
@@ -8,7 +8,7 @@ namespace NoPasaranTD
 {
     internal static class Program
     {
-        public const string SERVER_ADDRESS = "85.214.40.156";
+        public const string SERVER_ADDRESS = "localhost"; //85.214.40.156
         public const int SERVER_PORT = 31415;
 
         private static StaticDisplay display;
